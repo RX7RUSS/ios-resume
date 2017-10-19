eede6948c6775a1ac796aea16605c7ba16927d02
RX7RUSS <russ.savage5@gmail.com>
2017-10-18 21:43:37 -0500

    Changes to app file

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
@@ -20,5 +20,6 @@ const App = () => {
     </Provider>
   );
 };
+
 
 export default App;
